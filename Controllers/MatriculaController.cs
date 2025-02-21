d48dcd6fd84f0dec31cab743926d965c0db307ba
JaymeMoura <jaymeantonio34@gmail.com>
2025-02-21 16:44:58 -0300

    Finalizado a metade do matriculaController

--- Controllers/MatriculaController.cs
+++ Controllers/MatriculaController.cs
@@ -1,5 +1,7 @@
 ﻿using Cursos.API.Context;
+using Cursos.API.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cursos.API.Controllers;
 
@@ -13,7 +15,37 @@ public class MatriculaController : Controller
         _context = context;
     }
 
+    [HttpGet("{matriculaId}")]
+    public async Task<ActionResult> ObterMatricula(int matriculaId)
+    {
+        Matricula? matricula = await _context.Matriculas.FirstOrDefaultAsync(x => x.MatriculaId == matriculaId);
+        if (matricula == null)
+            return NotFound("Matricula não existe ou não foi encontrada!");
 
+        return Ok(matricula);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> MatricularAluno(int alunoId, int cursoId)
+    {
+        Aluno? aluno = await _context.Alunos.FirstOrDefaultAsync(x => x.AlunoId == alunoId);
+        if (aluno == null)
+            return NotFound("Aluno não existe ou não foi encontrado.");
+
+        Curso? curso = await _context.Cursos.FirstOrDefaultAsync(x => x.CursoId == cursoId);
+        if (curso == null)
+            return NotFound("Curso não encontrado.");
+
+        if (await _context.Matriculas.AnyAsync(x => x.CursoId == cursoId && x.AlunoId == alunoId))
+            return NotFound("Aluno já esta matriculado nesse curso!");
+
+        Matricula matricula = new(alunoId, cursoId);
+
+        _context.Matriculas.Add(matricula);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(ObterMatricula), new { matriculaId = matricula.MatriculaId}, matricula);
+    }
 
 
 }
--- Domain/Matricula.cs
+++ Domain/Matricula.cs
@@ -2,12 +2,19 @@
 
 public class Matricula
 {
-    public int MatriculaId { get; set; }
-    public int AlunoId { get; set; }
-    public Aluno Aluno { get; set; }
+    public int MatriculaId { get; private set; }
+    public int AlunoId { get; private set; }
+    public Aluno Aluno { get; private set; } = null!;
 
-    public int CursoId { get; set; }
-    public Curso Curso { get; set; }
+    public int CursoId { get; private set; }
+    public Curso Curso { get; private set; } = null!;
 
     public DateTime DataMatricula {  get; set; }
+
+    public Matricula(int alunoId, int cursoId)
+    {
+        AlunoId = alunoId;
+        CursoId = cursoId;
+        DataMatricula = DateTime.Now;
+    }
 }
